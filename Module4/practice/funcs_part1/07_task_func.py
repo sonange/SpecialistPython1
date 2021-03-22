d812560bc61bcdca19846c0549c7bc2615908e45
sonange <80748540+sonange@users.noreply.github.com>
2021-03-22 20:00:32 +0700

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,14 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+
+def sec_to_hour_minute_sec(sec_num):
+    hours = sec_num // 3600
+    minutes = (sec_num % 3600) // 60
+    seconds = (sec_num % 60) % 60
+    return f'{"0" + str(hours) if hours < 10 else str(hours)}:{"0" + str(minutes) if minutes < 10 else str(minutes)}:' \
+           f'{"0" + str(seconds) if seconds < 10 else str(seconds)}'
+
+
+print(sec_to_hour_minute_sec(3661))
