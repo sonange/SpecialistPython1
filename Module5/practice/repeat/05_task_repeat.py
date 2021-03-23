8c3fb1126d9304817a13aeb2c0044b508885d0bf
sonange <80748540+sonange@users.noreply.github.com>
2021-03-23 14:43:07 +0700

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,9 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def num_of_items_on_last_page(num_items, items_on_page):
+    return items_on_page if num_items % items_on_page == 0 else num_items % items_on_page
+
+
+print(num_of_items_on_last_page(8, 4))
+print(num_of_items_on_last_page(7, 4))
